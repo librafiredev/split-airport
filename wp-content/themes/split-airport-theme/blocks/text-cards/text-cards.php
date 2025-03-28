0becfbcbf9a6910eab8de0d086b6e6c97e0892ff
Lazar <lazar.lukic@librafire.com>
2025-03-28 09:14:39 +0100

    style desktop for text-cards section

--- wp-content/themes/split-airport-theme/blocks/text-cards/text-cards.php
+++ wp-content/themes/split-airport-theme/blocks/text-cards/text-cards.php
@@ -15,31 +15,32 @@ else: ?>
 
     ?>
 
-    <section class="text-cards-wrapper">
+    <section class="text-cards-wrapper" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/images/dots-pattern.svg);">
+
         <div class="container">
             <div class="text-cards-inner">
+                <img src="<?php echo get_template_directory_uri() ?>/assets/images/air-line.svg" class="text-cards-air-line" />
 
                 <div class="text-cards-top">
 
-                    <?php if( $title ): ?>
-
-                        <div class="text-cards-title">
-                            <h2>
-                                <?php echo $title; ?>
-                            </h2>
-                        </div><!-- .text-cards-title -->
+                    <div class="text-cards-top-item">
+                        <?php if( $title ): ?>
+                            <div class="text-cards-title">
+                                <h2>
+                                    <?php echo $title; ?>
+                                </h2>
+                            </div><!-- .text-cards-title -->
                         
-                    <?php endif; ?>
-
-                    <?php if( $text ): ?>
-
-                        <div class="text-cards-text">
-                            <p>
-                                <?php echo $text; ?>
-                            </p>
-                        </div><!-- .text-cards-text -->
+                        <?php endif; ?>
+                        <?php if( $text ): ?>
+                            <div class="text-cards-text">
+                                <p>
+                                    <?php echo $text; ?>
+                                </p>
+                            </div><!-- .text-cards-text -->
                         
-                    <?php endif; ?>
+                        <?php endif; ?>
+                    </div>
 
                 </div><!-- .text-cards-top -->
 
@@ -48,7 +49,7 @@ else: ?>
                     <div class="text-cards-items">
                 
                         <?php while ( have_rows('items') ) : the_row(); 
-                        
+                            $index = get_row_index();
                             $image = get_sub_field('image');
                             $image_type = get_sub_field('image_type');
                             $link = get_sub_field('link');
@@ -59,12 +60,13 @@ else: ?>
                                 $link_target = $link['target'] ? $link['target'] : '_self';
                             ?>
 
-                                <div class="text-card-item text-card-item-type-<?php echo $image_type; ?>" <?php echo ( $image_type == 'background' && $image )? 'style="background-image:url('.wp_get_attachment_image_url($image['ID']).');"': ''; ?>>
+                                <div class="text-card-item text-card-item-<?php echo $index; ?> text-card-item-type-<?php echo $image_type; ?>">
                                     <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
+                                        <div class="text-card-item-bg" <?php echo ( $image_type == 'background' && $image )? 'style="background-image:url('.wp_get_attachment_image_url($image['ID'], 'medium_large').');"': ''; ?> ></div>
 
                                         <?php if( $image_type != 'background' && $image ): ?>
 
-                                            <div class="text-card-item-image">
+                                            <div class="text-card-item-img-wrap text-card-item-<?php echo $image_type; ?>">
                                                 <?php echo ( isset($image['ID']) )? wp_get_attachment_image($image['ID'], 'full'):''; ?>
                                             </div><!-- .text-card-item-image -->
                                             
@@ -72,7 +74,7 @@ else: ?>
 
                                         <div class="text-card-item-text">
                                             <span><?php echo esc_html($link_title); ?></span>
-                                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow-white.svg" alt="arrow-white" />
+                                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow-white.svg" alt="arrow-white" class="text-card-item-arrow" />
                                         </div><!-- .text-card-item-text -->
 
                                     </a>
