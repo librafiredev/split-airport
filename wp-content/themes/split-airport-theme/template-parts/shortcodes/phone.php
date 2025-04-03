b1af8477864d790b6b024134ce92a42b101bd43b
Lazar <lazar.lukic@librafire.com>
2025-04-03 09:53:48 +0200

    style desktop for transportation section

--- wp-content/themes/split-airport-theme/template-parts/shortcodes/phone.php
+++ wp-content/themes/split-airport-theme/template-parts/shortcodes/phone.php
@@ -1,11 +1,11 @@
 <?php extract($args); ?>
 
-<div class="phone">
-    <div class="phone__left">
+<div class="phone-card">
+    <div class="phone-card__left">
         <?php echo file_get_contents(get_template_directory() . '/assets/images/phone.svg'); ?>
     </div>
-    <div class="phone__right">
-        <p class="phone__label"><?php esc_html_e('Telephone Number'); ?></p>
-        <p class="phone__number"><a href="tel:<?php echo $number; ?>"><?php echo $number; ?></a></p>
+    <div class="phone-card__right">
+        <p class="phone-card__label"><?php esc_html_e('Telephone Number'); ?></p>
+        <p class="phone-card__number"><a href="tel:<?php echo $number; ?>"><?php echo $number; ?></a></p>
     </div>
 </div>
