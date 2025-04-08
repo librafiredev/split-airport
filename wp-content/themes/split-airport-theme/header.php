b453e2daafed678032e37a85cc2aacfba79b96e9
Lazar <lazar.lukic@librafire.com>
2025-04-08 14:27:18 +0200

    implement acf option fields for warnings

--- wp-content/themes/split-airport-theme/header.php
+++ wp-content/themes/split-airport-theme/header.php
@@ -23,77 +23,57 @@
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">		
 		
-		<div class="site-warning-wrap">
-			<div class="container">
-				<div class="site-warning">
-					<!-- TODO: figure out what to do with colors for this icon (when warnings are open and there are multiple warnings with different type) -->
-					<div class="site-warning-icographics shared-warning" data-warning="info">
-						<div class="site-warning-icon-wrap">
-							<?php echo file_get_contents(get_template_directory() . '/assets/images/warning.svg') ?>
+		<?php if( have_rows('airport_warnings', 'option') ): ?>
+			<div class="site-warning-wrap">
+				<div class="container">
+					<div class="site-warning">
+						<?php 
+						$all_warnings = !empty(get_field('airport_warnings', 'option')) ? get_field('airport_warnings', 'option') : [];
+						$first_item_type = !empty($all_warnings[0]['type']) ? $all_warnings[0]['type'] : 'info';
+						?>
+						<div class="site-warning-icographics shared-warning" data-warning="<?php echo $first_item_type; ?>">
+							<div class="site-warning-icon-wrap">
+								<?php echo file_get_contents(get_template_directory() . '/assets/images/warning.svg') ?>
+							</div>
+							<div class="site-warning-type-text">
+								<?php echo get_field('airport_alert_text', 'option'); ?>
+							</div>
 						</div>
-						<div class="site-warning-type-text">Alerts</div>
-					</div>
 
-					<div class="site-warning-controls"><button type="button" class="site-warning-prev"></button> <span class="site-warning-controls-txt"><span class="site-warning-controls-current">1</span>&nbsp;<?php echo __('of'); ?>&nbsp;<span class="site-warning-controls-total"><!-- TODO: echo total from db here --></span></span><button type="button" class="site-warning-next"></button></div>
-						
+						<div class="site-warning-controls"><button type="button" class="site-warning-prev"></button> <span class="site-warning-controls-txt"><span class="site-warning-controls-current">1</span>&nbsp;<?php echo __('of'); ?>&nbsp;<span class="site-warning-controls-total"><?php echo count($all_warnings); ?></span></span><button type="button" class="site-warning-next"></button></div>
 
-					<div class="site-warning-items">
-						<div class="site-warning-items-inner">
+						<div class="site-warning-items">
+							<div class="site-warning-items-inner">
+								<?php while ( have_rows('airport_warnings', 'option') ) : the_row(); ?>
+									<?php $type = get_sub_field('type'); ?>
 
-							<!-- TODO: REPLACE PLACEHOLDER ITEMS -->
-
-							<!-- TODO: add apropriate class based on warning type (for each item) -->
-							<div class="site-warning-item current-warning" data-warning="info">
-								<div class="site-warning-icographics warning-item-icon">
-									<div class="site-warning-icon-wrap">
-										<?php echo file_get_contents(get_template_directory() . '/assets/images/warning.svg') ?>
+									<div class="site-warning-item current-warning" data-warning="<?php echo $type; ?>">
+										<div class="site-warning-icographics warning-item-icon">
+											<div class="site-warning-icon-wrap">
+												<?php echo file_get_contents(get_template_directory() . '/assets/images/warning.svg') ?>
+											</div>
+											<div class="site-warning-type-text"><?php echo get_field('airport_alert_text', 'option'); ?></div>
+										</div>
+										<div class="site-warning-right">
+											<span class="site-warning-item-title"><?php the_sub_field('title'); ?></span>
+											<div class="site-warning-item-text">
+												<?php the_sub_field('text'); ?>
+											</div>
+										</div>
 									</div>
-									<div class="site-warning-type-text">Alerts</div>
-								</div>
-								<div class="site-warning-right">
-									<span class="site-warning-item-title">today, feb 19</span>
-									<div class="site-warning-item-text">
-										Due to planned industrial action, there will be no flight from London to or from Split on Thursday 27 March
-									</div>
-								</div>
+									
+								<?php endwhile; ?>
+
 							</div>
-							<div class="site-warning-item" data-warning="danger">
-								<div class="site-warning-icographics warning-item-icon">
-									<div class="site-warning-icon-wrap">
-										<?php echo file_get_contents(get_template_directory() . '/assets/images/warning.svg') ?>
-									</div>
-									<div class="site-warning-type-text">Alerts</div>
-								</div>
-								<div class="site-warning-right">
-									<span class="site-warning-item-title">feb, 18</span>
-									<div class="site-warning-item-text">
-										Due to planned works, the M25 will be shut between Junctions 10 (Wisley Interchange) and 11 (Chertsey) from 9pm on Friday 21 March, until 6am on Monday 24 March. Passengers planning to use this part of the M25 should allow for extra time when travelling to Heathrow during this period. For more information, please visit National Highways’ website.
-									</div>
-								</div>
-							</div>
-							<div class="site-warning-item" data-warning="warning">
-								<div class="site-warning-icographics warning-item-icon">
-									<div class="site-warning-icon-wrap">
-										<?php echo file_get_contents(get_template_directory() . '/assets/images/warning.svg') ?>
-									</div>
-									<div class="site-warning-type-text">Alerts</div>
-								</div>
-								<div class="site-warning-right">
-									<span class="site-warning-item-title">feb, 17</span>
-									<div class="site-warning-item-text">
-										Due to planned works, there will be no flight from Paris to or from Split on Thursday 29 March
-									</div>
-								</div>
-							</div>
-
 						</div>
-					</div>
 
-					<button type="button" class="site-warning-expand"></button>
+						<button type="button" class="site-warning-expand"></button>
+					</div>
 				</div>
+				<div class="site-warning-overlay"></div>
 			</div>
-			<div class="site-warning-overlay"></div>
-		</div>
+		<?php endif; ?>
+
 		<div class="container logo-menu-wrapper">
 			<div class="site-header-inner">
 
--- wp-content/themes/split-airport-theme/includes/theme_functions.php
+++ wp-content/themes/split-airport-theme/includes/theme_functions.php
@@ -1,3 +1,16 @@
 <?php 
 
+if( function_exists('acf_add_options_page') ) {
+
+	acf_add_options_page(array(
+		'page_title' 	=> 'Warnings',
+		'menu_title'	=> 'Warnings',
+		'menu_slug' 	=> 'warning-settings',
+		'capability'	=> 'edit_posts',
+        'icon_url'      => 'dashicons-warning',
+		'redirect'		=> false
+	));
+
+}
+
 ?>
