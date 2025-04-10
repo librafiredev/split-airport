86b7bf05a5b8cae32cfdd736fc7e6437d6728754
Lazar <lazar.lukic@librafire.com>
2025-04-10 15:27:23 +0200

    implement acf fields for download schedule section

--- wp-content/themes/split-airport-theme/blocks/download-schedule/download-schedule.php
+++ wp-content/themes/split-airport-theme/blocks/download-schedule/download-schedule.php
@@ -13,46 +13,51 @@ else: ?>
         <div class="container">
             <div class="download-schedule-top">
                 <div class="download-schedule-top-left">
-                    <h2>Full flight schedule for the current year</h2>
+                    <h2><?php the_field('title'); ?></h2>
                 </div>
-                <div class="download-schedule-top-right">
-                    
-                    <?php
-                    // TODO: replace with the actual field
-                    $file_id = 303;
-                    ?>
-                    
-                    <?php if ( $file_id ): ?>
+
+                <?php if( have_rows('files') ): ?>
+                    <div class="download-schedule-top-right">
+                        <?php while ( have_rows('files') ) : the_row(); ?>
                         <?php
-
-                        $file_path = get_attached_file($file_id);
-                        $file_url = wp_get_attachment_url($file_id);
-                        $file_size = round(((filesize($file_path)) / 1024 / 1024), 2);
+                        $file_id = get_sub_field('file');
                         ?>
+                        
+                        <?php if ( $file_id ): ?>
+                            <?php
 
-                        <div class="download-schedule-tr-item">
-                            <div class="download-schedule-tri-icon-wrap">
-                                <?php echo file_get_contents(get_template_directory() . '/assets/images/fileIcon.svg'); ?>
-                            </div>
+                            $file_path = get_attached_file($file_id);
+                            $file_url = wp_get_attachment_url($file_id);
+                            $file_size = round(((filesize($file_path)) / 1024 / 1024), 2);
+                            ?>
 
-                            <div class="download-schedule-tri-main">
-                                <div class="download-schedule-tri-main-l">
-                                    <div class="download-schedule-tri-title"><?php echo ucfirst(basename($file_path)); ?></div>
-                                    <div class="download-schedule-tri-subtitle"><?php echo strtoupper(pathinfo($file_path, PATHINFO_EXTENSION)) . ', ' . $file_size . ' ' . 'MB'; ?></div>
+                            <div class="download-schedule-tr-item">
+                                <div class="download-schedule-tri-icon-wrap">
+                                    <?php echo file_get_contents(get_template_directory() . '/assets/images/fileIcon.svg'); ?>
                                 </div>
-                                <a href="<?php echo esc_url($file_url); ?>" download class="download-schedule-item-dl">
-                                    <?php esc_html_e('Download', 'split-aritport'); ?>
-                                </a>
+
+                                <div class="download-schedule-tri-main">
+                                    <div class="download-schedule-tri-main-l">
+                                        <div class="download-schedule-tri-title"><?php echo ucfirst(basename($file_path)); ?></div>
+                                        <div class="download-schedule-tri-subtitle"><?php echo strtoupper(pathinfo($file_path, PATHINFO_EXTENSION)) . ', ' . $file_size . ' ' . 'MB'; ?></div>
+                                    </div>
+                                    <a href="<?php echo esc_url($file_url); ?>" download class="download-schedule-item-dl">
+                                        <?php esc_html_e('Download', 'split-aritport'); ?>
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                    <?php endif; ?>
+                        <?php endif; ?>
 
+                        <?php endwhile; ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+
+            <?php if ( get_field('text') ): ?>
+                <div class="download-schedule-bottom">
+                    <?php the_field('text'); ?>
                 </div>
-            </div>
-
-            <div class="download-schedule-bottom">
-                Stated times are local take-off / landing times, from / to Split Airport. Flights without determinate period of validity operate throughout the season. Split Airport regularly updates timetable data, but can not guarantee that given information are complete or accurate. Split Airport hereby disclaims any liability for loss or damage caused by timetable errors or omissions.
-            </div>
+            <?php endif; ?>
         </div>
 
     </section><!-- .download-schedule-wrapper-->
