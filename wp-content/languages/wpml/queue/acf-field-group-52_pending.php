a7cf2584051980b658bf261a69ca94125b926cab
Lazar <lazar.lukic@librafire.com>
2025-04-11 12:52:57 +0200

    add autogenerated wmpl files after text-link-section

--- wp-content/languages/wpml/queue/acf-field-group-52_pending.php
+++ wp-content/languages/wpml/queue/acf-field-group-52_pending.php
@@ -1,2 +1,2 @@
 <?php
-return ['items'=>[]];
+return ['items'=>['Icon'=>['saveStringInDb'=>true,'cmp'=>['wpml-string-translation',1],'names'=>['field-466-label-817434295a673aed431435658b65d9a7'],'urls'=>[]]]];
