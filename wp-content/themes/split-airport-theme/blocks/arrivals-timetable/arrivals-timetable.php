823a977621b6e9440faeb79ed34eb4384c054031
Lazar <lazar.lukic@librafire.com>
2025-04-29 12:50:47 +0200

    SPA2-93: add back in header for mobile table arrivals table

--- wp-content/themes/split-airport-theme/blocks/arrivals-timetable/arrivals-timetable.php
+++ wp-content/themes/split-airport-theme/blocks/arrivals-timetable/arrivals-timetable.php
@@ -135,18 +135,18 @@ else:
                         ?>
                     </a>
                     <div class="arrivals-timetable__table-header">
-                        <span class="arrivals-timetable__table-name"><?php esc_html_e('Scheduled', 'split-airport') ?></span>
-                        <span class="arrivals-timetable__table-name"><?php esc_html_e('Estimated', 'split-airport') ?></span>
-                        <span class="arrivals-timetable__table-name flight-type"><?php echo $flightType === 'arrival' ? __('Arriving from', 'split-airport') :  __('Going to', 'split-airport')  ?></span>
-                        <span class="arrivals-timetable__table-name flight-info"><?php esc_html_e('Flight', 'split-airport') ?></span>
+                        <span class="arrivals-timetable__table-name flight__planned"><?php esc_html_e('Scheduled', 'split-airport') ?></span>
+                        <span class="arrivals-timetable__table-name flight__expected"><?php esc_html_e('Estimated', 'split-airport') ?></span>
+                        <span class="arrivals-timetable__table-name flight-type flight__arriving-from"><?php echo $flightType === 'arrival' ? __('Arriving from', 'split-airport') :  __('Going to', 'split-airport')  ?></span>
+                        <span class="arrivals-timetable__table-name flight-info flight__flight"><?php esc_html_e('Flight', 'split-airport') ?></span>
 
                         <?php if (isset($_GET['flightType']) && $_GET['flightType'] === 'departure'): ?>
 
-                            <span class="arrivals-timetable__table-name gate"><?php esc_html_e('Gate', 'split-airport') ?></span>
+                            <span class="arrivals-timetable__table-name gate flight__baggage-gate"><?php esc_html_e('Gate', 'split-airport') ?></span>
 
                         <?php endif; ?>
 
-                        <span class="arrivals-timetable__table-name"><?php esc_html_e('Status', 'split-airport') ?></span>
+                        <span class="arrivals-timetable__table-name flight__baggage-status"><?php esc_html_e('Status', 'split-airport') ?></span>
                     </div>
 
                     <div class="arrivals-timetable__table-flights">
