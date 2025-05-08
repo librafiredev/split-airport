c93fe6fa4b77731804684a49e290bb33829175a6
Lazar <lazar.lukic@librafire.com>
2025-05-08 13:08:25 +0200

    implement basic airport map sidebar

--- wp-content/themes/split-airport-theme/blocks/airport-map/airport-map.js
+++ wp-content/themes/split-airport-theme/blocks/airport-map/airport-map.js
@@ -1,5 +1,8 @@
 $(function () {
 
-
+    $('.map-sidebar-level-1>li>.map-sidebar-btn').on('click', function () {
+        $(this).siblings('ul').eq(0).stop().slideToggle();
+        $(this).toggleClass('map-sidebar-open-sub');
+    });
 
 });
