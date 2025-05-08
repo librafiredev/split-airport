45320031efb864d62fb2a0dd5c87232f2bd1ed5a
Lazar <lazar.lukic@librafire.com>
2025-05-08 14:34:45 +0200

    disable autocomplete on search

--- wp-content/themes/split-airport-theme/template-parts/blocks/arrivals-timetable-search.php
+++ wp-content/themes/split-airport-theme/template-parts/blocks/arrivals-timetable-search.php
@@ -25,7 +25,7 @@ for ($i = 0; $i < 5; $i++) {
     <div class="arrivals-timetable-search__input-wrap">
         <div class="arrivals-timetable-search__input">
             <?php echo file_get_contents(get_template_directory() . '/assets/images/search-icon.svg');  ?>
-            <input name="search" type="text" placeholder="<?php esc_html_e('Find your Flights', 'split-airport'); ?>" />
+            <input name="search" type="text" placeholder="<?php esc_html_e('Find your Flights', 'split-airport'); ?>" autocomplete="off" />
             <div class="loader-search"></div>
         </div>
         <div style="display: none;" class="arrivals-timetable-search__bottom">
