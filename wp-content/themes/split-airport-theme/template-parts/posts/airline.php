043227300d75831e0aed3cd94a08a7682a13c5e7
Lazar <lazar.lukic@librafire.com>
2025-06-03 15:27:49 +0200

    Revert "55-web-check-in: add opens and closes fields to airlines"
    This reverts commit cdec8b36b6ca7da35df9623942056bb7955d255b.

--- wp-content/themes/split-airport-theme/template-parts/posts/airline.php
+++ wp-content/themes/split-airport-theme/template-parts/posts/airline.php
@@ -3,8 +3,6 @@ $logo = get_the_post_thumbnail();
 $url = get_field('url', get_the_ID());
 $type = get_field('type', get_the_ID());
 $airlineUnsupportedText = get_field('airline_unsupported_text', 'options');
-$opensText = get_field('opens', get_the_ID());
-$closesText = get_field('closes', get_the_ID());
 ?>
 
 <?php if (!empty($url)): ?>
@@ -24,29 +22,18 @@ $closesText = get_field('closes', get_the_ID());
 
             <div>
                 <h3 class="airline__title"><?php the_title(); ?></h3>
-                <span class="airlline__subtitle">
-                    <?php if ($type === 'unsupported'): ?>
+                <?php if ($type === 'unsupported'): ?>
+                    <span class="airlline__url-mobile">
                         <?php echo $airlineUnsupportedText; ?>
-                    <?php elseif ($type === 'supported'): ?>
-                        <?php if ($opensText) : ?>
-                            <span class="airlline__subtitle-item">
-                                <?php echo $opensText ?>
-                            </span>
-                        <?php endif; ?>
-                        <?php if ($closesText) : ?>
-                            <span class="airlline__subtitle-item">
-                                <?php echo $closesText ?>
-                            </span>
-                        <?php endif; ?>
-                    <?php endif; ?>
-                </span>
+                    </span>
+                <?php endif; ?>
             </div>
         </div>
 
         <div class="airline__right">
             <div class="airline__url">
                 <?php if ($type === 'unsupported'): ?>
-                    <p class="airline__text"><?php echo file_get_contents(get_template_directory() . '/assets/images/link-arrow.svg'); ?></p>
+                    <p class="airline__text"><span class="airline-external"><?php echo $airlineUnsupportedText; ?></span><?php echo file_get_contents(get_template_directory() . '/assets/images/link-arrow.svg'); ?></p>
                 <?php else: ?>
                     <?php echo file_get_contents(get_template_directory() . '/assets/images/arrow-right.svg'); ?>
                 <?php endif; ?>
