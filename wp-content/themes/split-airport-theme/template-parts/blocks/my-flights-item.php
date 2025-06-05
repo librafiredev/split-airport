645a62db97a3b8ed2e6f14352dcea4eb49d08f9b
Lazar <lazar.lukic@librafire.com>
2025-06-05 10:22:54 +0200

    prevent supplying nullable to date formatter in my flights item

--- wp-content/themes/split-airport-theme/template-parts/blocks/my-flights-item.php
+++ wp-content/themes/split-airport-theme/template-parts/blocks/my-flights-item.php
@@ -8,7 +8,7 @@
     ?>
  <div>
      <strong><?php echo $flight['flight_number']; ?></strong>
-     <?php echo $destinationDisplay; ?> <strong><?php echo  $flight['esttime'] ? DateTimeFlight::formatTimeTableView($flight['esttime']) : DateTimeFlight::formatTimeTableView($flight['schtime']); ?></strong>
+     <?php echo $destinationDisplay; ?> <strong><?php echo  $flight['esttime'] ? DateTimeFlight::formatTimeTableView($flight['esttime']) : ($flight['schtime'] ? DateTimeFlight::formatTimeTableView($flight['schtime']) : ''); ?></strong>
      <?php if ($type === 'departure'): ?> <?php esc_html_e('Gate', 'split-airport'); ?>
          <strong><?php echo $flight['gate']; ?></strong> <?php endif; ?>
      <strong><?php echo $flight['comment']; ?></strong>
