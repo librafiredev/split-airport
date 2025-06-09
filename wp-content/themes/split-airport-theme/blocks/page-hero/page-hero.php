98f227439aef75812ffd2a3a57c1d3031d51f9b3
librafiredev <branislav.pavkovic@librafire.com>
2025-06-09 15:44:00 +0200

    95-follow-this-flight-tab-s-letom-se-ne-prikazuje-na-večini-headera-na-webu-passenger  & Business Logo link fix

--- wp-content/themes/split-airport-theme/blocks/page-hero/page-hero.php
+++ wp-content/themes/split-airport-theme/blocks/page-hero/page-hero.php
@@ -5,44 +5,44 @@
 */
 
 if (isset($block['data']['preview_image_help'])) :
-    echo '<img src="' . $block['data']['preview_image_help'] . '" style="width:100%; height:auto;">';
+    echo '<img src="' . esc_url($block['data']['preview_image_help']) . '" style="width:100%; height:auto;">';
 else:
     $working_hours = get_field('working_hours');
 
-      $follow_flights_pages = get_field('follow_flights_pages', 'options') ?? [];
-    
-    if ( !is_array($follow_flights_pages) ) {
-        $follow_flights_pages = [ $follow_flights_pages ];
-    }
+    $follow_flights_pages = get_field('business_homepage', 'options') ?? [];
+    $excluded_pages = array_map(fn($post) => $post->ID, $follow_flights_pages);
 ?>
 
     <section class="page-hero-wrapper">
 
         <div class="page-hero-wrapper-inner">
-            <div class="page-hero-img-wrap"><div class="page-hero-img-pattern" style="background-image: url(<?php echo get_template_directory_uri() ?>/assets/images/hero-pattern.png);"></div><?php echo wp_get_attachment_image(get_field('background'), 'full',); ?></div>
+            <div class="page-hero-img-wrap">
+                <div class="page-hero-img-pattern" style="background-image: url(<?php echo esc_url(get_template_directory_uri() . '/assets/images/hero-pattern.png'); ?>);"></div>
+                <?php echo wp_get_attachment_image(get_field('background'), 'full'); ?>
+            </div>
             <div class="page-hero-cutout-wrap">
                 <svg version="1.2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1439 71" width="1439" height="71" class="page-hero-cutout">
                     <path d="m682.7 0h-682.7v716h1445v-645h-611.4c-18.5 0-36.5-6.1-51.3-17.3l-48.3-36.5c-14.8-11.2-32.8-17.2-51.3-17.2z" />
                 </svg>
             </div>
 
-            <?php if(in_array(get_the_ID(), $follow_flights_pages)) get_template_part('template-parts/blocks/my-flights'); ?>
+            <?php 
+            if (!in_array(get_the_ID(), $excluded_pages)) {
+                get_template_part('template-parts/blocks/my-flights'); 
+            }
+            ?>
         </div>
-
 
         <div class="container">
 
             <?php if (get_field('title')): ?>
-            
                 <h1 class="page-hero-title"><?php the_field('title'); ?></h1>
-
             <?php endif; ?>
 
             <?php if ($working_hours): ?>
-
-                <p class="page-hero-working-hours"><?php echo $working_hours; ?></p>
-
+                <p class="page-hero-working-hours"><?php echo esc_html($working_hours); ?></p>
             <?php endif; ?>
+
         </div>
     </section><!-- .page-hero-wrapper-->
 
--- wp-content/themes/split-airport-theme/header.php
+++ wp-content/themes/split-airport-theme/header.php
@@ -6,7 +6,6 @@
  *
  * @package Split
  */
-
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -15,53 +14,68 @@
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php wp_head(); ?>
-    <?php include_once(get_template_directory(). '/includes/fonts.php'); ?>
+	<?php include_once(get_template_directory(). '/includes/fonts.php'); ?>
 	<meta name="theme-color" content="#010101">
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">		
-	<div class="container logo-menu-wrapper">
+		<div class="container logo-menu-wrapper">
 			<div class="site-header-inner">
-				
-				<?php if( function_exists('get_field') ): ?>
 
-					<?php $logo = get_field('logo','option'); ?>
+				<?php if( function_exists('get_field') ): ?>
+					<?php 
+						$logo = get_field('logo', 'option'); 
+						$business_homepages = get_field('business_homepage', 'option');
+						$current_page_id = get_queried_object_id();
+						$logo_link = home_url('/');
+						$currentLanguage = apply_filters('wpml_current_language', null);
+						$locale = $currentLanguage === 'hr' ? 'hr_HR.UTF-8' : 'en_US.UTF-8';
+
+						if ( is_array($business_homepages) ) {
+							foreach ( $business_homepages as $page ) {
+									if ( $page instanceof WP_Post && $page->ID === $current_page_id ) {
+											if ( $currentLanguage === 'hr' ) {
+													$logo_link = home_url('/hr/business/');
+											} else {
+													$logo_link = home_url('/business/');
+											}
+											break;
+									}
+							}
+					}
+
+					?>
 
 					<?php if( $logo ): ?>
-
+						<!-- Desktop Logo -->
 						<div class="site-branding-main-logo site-branding">
 							<div class="site-title">
-								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-									<img src="<?php echo( esc_url( $logo ) ); ?>" alt="<?php echo( esc_attr( get_bloginfo( 'title' ) ) ); ?>"/>
+								<a href="<?php echo esc_url( $logo_link ); ?>" rel="home">
+									<img src="<?php echo esc_url( $logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'title' ) ); ?>"/>
 								</a>
 							</div>
 						</div><!-- .site-branding -->
-						
 					<?php endif; ?>
-
 				<?php endif; ?>
-			
+
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<?php if( function_exists('get_field') ): ?>
-
-						<?php $logo = get_field('logo','option'); ?>
+						<?php 
+							$logo = get_field('logo', 'option'); 
+						?>
 
 						<?php if( $logo ): ?>
-
 							<div class="mobile-nav-logo">
-								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-									<img src="<?php echo( esc_url( $logo ) ); ?>" alt="<?php echo( esc_attr( get_bloginfo( 'title' ) ) ); ?>"/>
+								<a href="<?php echo esc_url( $logo_link ); ?>" rel="home">
+									<img src="<?php echo esc_url( $logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'title' ) ); ?>"/>
 								</a>
 							</div>
-							
 						<?php endif; ?>
-
 					<?php endif; ?>
-					
-					<?php
 
+					<?php
 						wp_nav_menu(
 							array(
 								'theme_location' 		=> 	'primary',
@@ -70,11 +84,9 @@
 								'container_class'		=>	'main-menu-container'
 							)
 						);
+					?>
 
-						?>
-					
 					<?php custom_language_selector(); ?>
-					
 				</nav><!-- #site-navigation -->
 
 				<div class="menu-toggle-wrapper">
@@ -84,7 +96,7 @@
 						<span></span>
 					</a>
 				</div>
-			</div> <!-- /.row justify-content-between -->
+			</div> <!-- /.site-header-inner -->
 		</div> <!-- /.container logo-menu-wrapper -->
 
 		<?php if( have_rows('airport_warnings', 'option') ): ?>
@@ -95,28 +107,34 @@
 						$all_warnings = !empty(get_field('airport_warnings', 'option')) ? get_field('airport_warnings', 'option') : [];
 						$first_item_type = !empty($all_warnings[0]['type']) ? $all_warnings[0]['type'] : 'info';
 						?>
-						<div class="site-warning-icographics shared-warning" data-warning="<?php echo $first_item_type; ?>">
+						<div class="site-warning-icographics shared-warning" data-warning="<?php echo esc_attr($first_item_type); ?>">
 							<div class="site-warning-icon-wrap">
-								<?php echo file_get_contents(get_template_directory() . '/assets/images/warning.svg') ?>
+								<?php echo file_get_contents(get_template_directory() . '/assets/images/warning.svg'); ?>
 							</div>
 							<div class="site-warning-type-text">
-								<?php echo get_field('airport_alert_text', 'option'); ?>
+								<?php echo esc_html(get_field('airport_alert_text', 'option')); ?>
 							</div>
 						</div>
 
-						<div class="site-warning-controls"><button type="button" class="site-warning-prev"></button> <span class="site-warning-controls-txt"><span class="site-warning-controls-current">1</span>&nbsp;<?php echo __('of'); ?>&nbsp;<span class="site-warning-controls-total"><?php echo count($all_warnings); ?></span></span><button type="button" class="site-warning-next"></button></div>
+						<div class="site-warning-controls">
+							<button type="button" class="site-warning-prev"></button> 
+							<span class="site-warning-controls-txt">
+								<span class="site-warning-controls-current">1</span>&nbsp;<?php echo __('of'); ?>&nbsp;<span class="site-warning-controls-total"><?php echo count($all_warnings); ?></span>
+							</span>
+							<button type="button" class="site-warning-next"></button>
+						</div>
 
 						<div class="site-warning-items">
 							<div class="site-warning-items-inner">
 								<?php while ( have_rows('airport_warnings', 'option') ) : the_row(); ?>
 									<?php $type = get_sub_field('type'); ?>
 
-									<div class="site-warning-item current-warning" data-warning="<?php echo $type; ?>">
+									<div class="site-warning-item current-warning" data-warning="<?php echo esc_attr($type); ?>">
 										<div class="site-warning-icographics warning-item-icon">
 											<div class="site-warning-icon-wrap">
-												<?php echo file_get_contents(get_template_directory() . '/assets/images/warning.svg') ?>
+												<?php echo file_get_contents(get_template_directory() . '/assets/images/warning.svg'); ?>
 											</div>
-											<div class="site-warning-type-text"><?php echo get_field('airport_alert_text', 'option'); ?></div>
+											<div class="site-warning-type-text"><?php echo esc_html(get_field('airport_alert_text', 'option')); ?></div>
 										</div>
 										<div class="site-warning-right">
 											<span class="site-warning-item-title"><?php the_sub_field('title'); ?></span>
@@ -125,9 +143,7 @@
 											</div>
 										</div>
 									</div>
-									
 								<?php endwhile; ?>
-
 							</div>
 						</div>
 
@@ -137,9 +153,7 @@
 				<div class="site-warning-overlay"></div>
 			</div>
 		<?php endif; ?>
-		
+
 	</header><!-- #masthead /.site-header -->
 
 	<div id="content" class="site-content">
-
-		
